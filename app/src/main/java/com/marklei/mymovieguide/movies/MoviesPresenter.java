cb3d469c5bf77f21128114a7c3ad8f503a86db9e
Mark <lh.t-mac@qq.com>
2018-04-17 17:13:43 +0800

    统一Dagger依赖版本

--- app/src/main/java/com/marklei/mymovieguide/movies/MoviesPresenter.java
+++ app/src/main/java/com/marklei/mymovieguide/movies/MoviesPresenter.java
@@ -1,8 +1,12 @@
 package com.marklei.mymovieguide.movies;
 
+import com.marklei.mymovieguide.data.Movie;
+import com.marklei.mymovieguide.data.source.MoviesDataSource;
 import com.marklei.mymovieguide.data.source.MoviesRepository;
 import com.marklei.mymovieguide.di.ActivityScoped;
 import com.marklei.mymovieguide.movies.sorting.SortType;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -11,7 +15,9 @@ public class MoviesPresenter implements MoviesContract.Presenter {
 
     private final MoviesRepository mMoviesRepository;
 
-    private MoviesContract.View mView;
+    private MoviesContract.View view;
+
+    private boolean mFirstLoad = true;
 
     @Inject
     MoviesPresenter(MoviesRepository moviesRepository) {
@@ -20,7 +26,41 @@ public class MoviesPresenter implements MoviesContract.Presenter {
 
     @Override
     public void loadMovies(boolean forceUpdate) {
+        // Simplification for sample: a network reload will be forced on first load.
+        loadTasks(forceUpdate || mFirstLoad, true);
+        mFirstLoad = false;
+    }
 
+    /**
+     * @param forceUpdate   Pass in true to refresh the data in the {@link MoviesDataSource}
+     * @param showLoadingUI Pass in true to display a loading icon in the UI
+     */
+    private void loadTasks(boolean forceUpdate, final boolean showLoadingUI) {
+        if (showLoadingUI) {
+            if (isViewAttached()) {
+                view.setLoadingIndicator(true);
+            }
+        }
+        if (forceUpdate) {
+            mMoviesRepository.refreshMovies();
+        }
+
+        mMoviesRepository.getMovies(new MoviesDataSource.LoadMoviesCallback() {
+
+            @Override
+            public void onMoviesLoaded(List<Movie> movies) {
+
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+
+            }
+        });
+    }
+
+    private boolean isViewAttached() {
+        return view != null;
     }
 
     @Override
@@ -30,12 +70,12 @@ public class MoviesPresenter implements MoviesContract.Presenter {
 
     @Override
     public void takeView(MoviesContract.View view) {
-        mView = view;
+        this.view = view;
         loadMovies(false);
     }
 
     @Override
     public void dropView() {
-        mView = null;
+        view = null;
     }
 }
